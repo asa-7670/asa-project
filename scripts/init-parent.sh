28cb95dbf1b94bdd445781119e41782b9d9d2008
abdelhalim.saidi <abdelhalim.saidi@sis-marches.com>
2018-12-29 17:29:10 +0100

    init parent asa-app project

---
+++ scripts/init-parent.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Parent project
+	mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes 
+	-DarchetypeArtifactId=maven-archetype-quickstart 
+	-DarchetypeVersion=1.1 
+	-DgroupId=com.asa.app 
+	-DartifactId=asa-app 
+	-Dversion=1.0-SNAPSHOT
+
+# Model module
+# Batch module
+# Consumer module
+# Business module
+# Rest module
+
+
