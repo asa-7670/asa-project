d9bdfda39644178fc179ee33f3d885e76d6526ac
abdelhalim.saidi <abdelhalim.saidi@sis-marches.com>
2018-12-29 18:23:44 +0100

    add module: asa-model

--- scripts/init-project.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Parent project
-mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.1 -DgroupId=com.asa.app -DartifactId=asa-app -Dversion=1.0-SNAPSHOT
-
-
-
